5985a73438f245efda2fa445cf73c4e434b6e796
Gil <frogvsdogvsmarsmelo@gmail.com>
2022-08-23 13:03:24 +0300

    Added main file and changed the contact class props to private

--- PhoneBook/src/Contact.ts
+++ PhoneBook/src/Contact.ts
@@ -1,5 +1,20 @@
 class Contact {
-    constructor(public id: number, public name: string, public address: string, public phones: string[]) { }
+    constructor(private _id: number, private _name: string, private _address: string, private _phones: string[]) { }
+    get id(): number {
+        return this._id
+    }
+    get name(): string {
+        return this._name
+    }
+    get address(): string {
+        return this._address
+    }
+    get phones(): string[] {
+        return this._phones
+    }
+    setMyphones(input: string) {
+        this._phones.push(input)
+    }
 
 }
 export default Contact
--- PhoneBook/src/IPhoneBook.ts
+++ PhoneBook/src/IPhoneBook.ts
@@ -4,7 +4,7 @@ interface IPhoneBook {
     add(contact: Contact): number
     addPhone(id: number, phone: string): void
     get(id: number): Contact | undefined
-    get(name: string): Contact | undefined
+    get(name: string): Contact[] | undefined
     remove(id: number): Contact | undefined
 }
 
